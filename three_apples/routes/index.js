8ded45b8651c8449f1806ef99d91c7a7a7e42c24
АзимБулатов <azimbulatov21@gmail.com>
2024-01-10 21:45:43 +0300

    пункт 4.3

--- three_apples/routes/index.js
+++ three_apples/routes/index.js
@@ -7,15 +7,27 @@ router.get('/', function(req, res, next) {
   });
   
 router.get('/Rapple', function(req, res, next) {
-  res.send("<h1>Страница красного яблока </h1>")
+res.render('apple', {
+title: "Красное яблоко",
+picture: "images/red_apple.jpg",
+desc: "Я не знаю что можно сказать про красное яблоко. Оно как бы есть, оно как бы вкусное, ну и в общем то все."
+});
 });
   
 router.get('/Yapple', function(req, res, next) {
-  res.send("<h1>Страница желтого яблока </h1>")
+res.render('apple', {
+title: "Желтое яблоко",
+picture: "images/yellow_apple.jpg",
+desc: "Пожалуй самое сладкое яблоко из всех что я пробовал. Но моим любимым яблоком остается красное"
+});
 });
 
 router.get('/Gapple', function(req, res, next) {
-    res.send("<h1>Страница зеленого яблока </h1>")
+res.render('apple', {
+title: "Зеленое яблоко",
+picture: "images/green_apple.jpg",
+desc: "Зеленое яблоко самое кислое из всех. Я бы съел его наверное только при условии, что мне бы мне заплатили за это."
+});
 });
 
 module.exports = router;
