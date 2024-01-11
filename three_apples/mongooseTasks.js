ceae14de5c969c59425a4a402a225d572835cf24
АзимБулатов <azimbulatov21@gmail.com>
2024-01-11 14:00:05 +0300

    пункт 7.2

--- three_apples/mongooseTasks.js
+++ three_apples/mongooseTasks.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-const Apple = mongoose.model('Apple', { name: String });
+  
+const AppleSchema = new mongoose.Schema( {name: String} );
+AppleSchema.methods.brew = function() {
+console.log(this.name + ' издает хруст');
+}
+const Apple = mongoose.model('Apple', AppleSchema);
 const welding = new Apple({ name: 'Семиренка' });
-
+  
 welding.save()
-  .then(() => console.log('Хруст'))
-  .catch((err) => console.log(err));
+  .then(() => welding.brew())
+  .catch((err) => console.log(err))
