24a15ea4c01f5115cf37be7d8dcdcf28eb155792
АзимБулатов <azimbulatov21@gmail.com>
2024-01-11 10:33:42 +0300

    пункт 6.1

---
+++ three_apples/createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("threeapples");
+database.dropDatabase()
+database = client.db("threeapples");
+const apples = database.collection("apples");
+const result = await apples.insertOne({name:"Гольден"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
