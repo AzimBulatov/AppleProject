967c3a2118a07f06c8325c236eb98b86fe9f2bbe
АзимБулатов <azimbulatov21@gmail.com>
2024-01-11 23:38:27 +0300

    пункт 8.3

--- three_apples/routes/apples.js
+++ three_apples/routes/apples.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Apple = require("../models/apple").Apple
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с apples');
 });
 
-router.get('/Rapple', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get('/Yapple', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get('/Gapple', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });    
+router.get("/:nick", async (req, res, next) => {
+    try {
+      const apple = await Apple.findOne({ nick: req.params.nick });
+      console.log(apple);
+      if (!apple) {
+        throw new Error("Нет такого яблока");
+      }
+      res.render('apple', {
+        title: apple.title,
+        picture: apple.avatar,
+        desc: apple.desc
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+       
 module.exports = router;
