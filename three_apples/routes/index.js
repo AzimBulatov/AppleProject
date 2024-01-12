12ba204cf4dfb2ea5e60e454327b012f579f3dca
АзимБулатов <azimbulatov21@gmail.com>
2024-01-12 22:48:15 +0300

    пункт 9.2

--- three_apples/routes/index.js
+++ three_apples/routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const Apple = require("../models/apple").Apple;
-
+var express = require('express')
+var router = express.Router()
+var Apple = require("../models/apple").Apple
 /* GET home page. */
+
+
 router.get('/', async (req, res, next) => {
   try {
-    const menu = await Apple.find({}, { _id: 0, title: 1, nick: 1 });
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
-  } catch (err) {
-    next(err);
-  }
+      const menu = await Apple.find({}, { _id: 0, title: 1, nick: 1 }).exec();
+      res.cookie('greeting', 'Hi!!!').render('index', { 
+        title: 'Express',
+        menu: menu 
+      });
+ } catch (err) {
+next(err);
+}
 });
 
 module.exports = router;
