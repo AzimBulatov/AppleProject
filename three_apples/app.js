b8e8842396e125ca128dcd26d51cb30d11b80d52
АзимБулатов <azimbulatov21@gmail.com>
2024-01-13 18:31:02 +0300

    пункт 11.5

--- three_apples/app.js
+++ three_apples/app.js
@@ -3,47 +3,78 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/threeapples')
-const session = require('express-session');
-const app = express();
-var Appple = require("./models/apple").Apple
+var bodyParser = require('body-parser');
+//var mongoose = require('mongoose');
+var session = require("express-session")
+// var Apple = require("./models/Apple").Apple
+//mongoose.connect('mongodb://127.0.0.1/Apples');
+var mysql2 = require('mysql2/promise');
+var MySQLStore = require('express-mysql-session')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apples = require('./routes/apples');
 
+var app = express();
 
+var options = {
+  host : '127.0.0.1',
+  port: '3306',
+  user : 'root',
+  password : '1234',
+  database: 'threeapples'
+  };
+var connection = mysql2.createPool(options)
+var sessionStore = new MySQLStore( options, connection);
 
 // view engine setup
-app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs',require('ejs-locals'));
+
+app.use(session({
+    secret: 'ThreeApples',
+    key: 'sid',
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { path: '/',
+      httpOnly: true,
+      maxAge: 60*1000
+    }
+}));
+
 
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var MongoStore = require('connect-mongo');
-app.use(session({
-  secret: "ThreeApples",
-  cookie:{maxAge:60*1000},
-  resave: true,
-  saveUninitialized: true,
-  store: MongoStore.create({mongoUrl: 'mongodb://localhost/threeapples'})
-  }))
-app.use(function(req,res,next){
-  req.session.counter = req.session.counter +1 || 1
-  next()
-  })
-    
-app.use(require("./middleware/createMenu.js"))
+// var MongoStore = require('connect-mongo');
+// app.use(session({
+//   secret: "ThreeApples",
+//   cookie:{maxAge:60*1000},
+//   resave: true,
+//   saveUninitialized: true,
+//   store: MongoStore.create({mongoUrl: 'mongodb://localhost/apples'})
+//   }))
+
+
+  app.use(function(req,res,next){
+    req.session.counter = req.session.counter +1 || 1
+    next()
+})
+   
+
 app.use(require("./middleware/createUser.js"))
+app.use(require("./middleware/createMenu.js"));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/apples', apples);
+
 
 
 // catch 404 and forward to error handler
@@ -56,13 +87,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
-
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title:""});
-
+  res.render('error', {title:"Упс... яблоко потерялось"});
 });
 
 module.exports = app;
