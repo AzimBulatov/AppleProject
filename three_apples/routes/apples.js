7f2a26020d830952230875dccf7f3659bee56d19
АзимБулатов <azimbulatov21@gmail.com>
2024-01-14 21:35:42 +0300

    10.6 и исправление ошибок

--- three_apples/routes/apples.js
+++ three_apples/routes/apples.js
@@ -15,7 +15,7 @@ router.get('/:nick', async (req, res, next) => {
             Apple.find({}, { _id: 0, title: 1, nick: 1 }).exec()
         ]);
 
-        if (!apple) throw new Error("Нет такого чая");
+        if (!apple) throw new Error("Нет такого яблока");
 
         res.render('apple', {
             title: apple.title,
